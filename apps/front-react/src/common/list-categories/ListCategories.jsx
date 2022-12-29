dc87d8f9dac3bc6bc5a28bcc281daac4ad5854cc
chupinal <alexischup@gmail.com>
2022-12-29 11:46:08 +0100

    Logo for categories, image import static

--- apps/front-react/src/common/list-categories/ListCategories.jsx
+++ apps/front-react/src/common/list-categories/ListCategories.jsx
@@ -1,14 +1,67 @@
 import React, { useEffect, useState } from "react";
 import "./ListCategories.css";
+
+import Default from "../../assets/png/common/default.png";
+import AIMachineLearning from "../../assets/png/categories/ai.png";
+import Cryptocurrencies from "../../assets/png/categories/cryptocurrrencies.png";
+import DataAndAnalytics from "../../assets/png/categories/data-and-analytics.png";
+import Finances from "../../assets/png/categories/finances.png";
+import GraphicsAndDesigns from "../../assets/png/categories/graphics-and-designs.png";
+import Marketing from "../../assets/png/categories/marketing.png";
+import Productivity from "../../assets/png/categories/productivity.png";
+import Programming from "../../assets/png/categories/programming.png";
+import Stockmarket from "../../assets/png/categories/stock-market.png";
+
 import { AXIOS } from "../../app/axios-http";
 import { useLocation } from "react-router-dom";
 import Spinner from "../generic/spinner/Spinner";
 import CategoryItem from "./category-item/CategoryItem";
 
+const baseUrlImage = "../../../assets/png/categories/";
+
 export default function ListCategories() {
   let [isRequesting, setIsRequesting] = useState(false);
   let [categories, setCategories] = useState([]);
   const location = useLocation();
+
+  const returnImageAccordingToCategory = (categoryUrl) => {
+    let imageSrc = Default;
+
+    switch (categoryUrl) {
+      case "ai":
+        imageSrc = AIMachineLearning;
+        break;
+      case "cryptocurrencies":
+        imageSrc = Cryptocurrencies;
+        break;
+      case "data-and-analytics":
+        imageSrc = DataAndAnalytics;
+        break;
+      case "finances":
+        imageSrc = Finances;
+        break;
+      case "graphics-and-designs":
+        imageSrc = GraphicsAndDesigns;
+        break;
+      case "marketing":
+        imageSrc = Marketing;
+        break;
+      case "productivity":
+        imageSrc = Productivity;
+        break;
+      case "programming":
+        imageSrc = Programming;
+        break;
+      case "stock-market":
+        imageSrc = Stockmarket;
+        break;
+
+      default:
+        break;
+    }
+
+    return imageSrc;
+  };
 
   useEffect(() => {
     if (location.state !== null) {
@@ -33,6 +86,8 @@ export default function ListCategories() {
           {categories.slice(0, 3).map((category, index) => {
             return (
               <CategoryItem
+                // imageSrc={baseUrlImage + category.url + ".png"}
+                imageSrc={returnImageAccordingToCategory(category.url)}
                 categories={categories}
                 key={category.id}
                 category={category}
@@ -47,6 +102,8 @@ export default function ListCategories() {
           {categories.slice(0, categories.length).map((category, index) => {
             return (
               <CategoryItem
+                // imageSrc={baseUrlImage + category.url + ".png"}
+                imageSrc={returnImageAccordingToCategory(category.url)}
                 categories={categories}
                 key={category.id}
                 category={category}
@@ -69,6 +126,8 @@ export default function ListCategories() {
           {categories.slice(3, 6).map((category, index) => {
             return (
               <CategoryItem
+                // imageSrc={baseUrlImage + category.url + ".png"}
+                imageSrc={returnImageAccordingToCategory(category.url)}
                 categories={categories}
                 key={category.id}
                 category={category}
@@ -83,6 +142,8 @@ export default function ListCategories() {
           {categories.slice(3, categories.length).map((category, index) => {
             return (
               <CategoryItem
+                // imageSrc={baseUrlImage + category.url + ".png"}
+                imageSrc={returnImageAccordingToCategory(category.url)}
                 categories={categories}
                 key={category.id}
                 category={category}
@@ -105,6 +166,8 @@ export default function ListCategories() {
           {categories.slice(6, 9).map((category, index) => {
             return (
               <CategoryItem
+                // imageSrc={baseUrlImage + category.url + ".png"}
+                imageSrc={returnImageAccordingToCategory(category.url)}
                 categories={categories}
                 key={category.id}
                 category={category}
@@ -119,6 +182,8 @@ export default function ListCategories() {
           {categories.slice(6, categories.length).map((category, index) => {
             return (
               <CategoryItem
+                // imageSrc={baseUrlImage + category.url + ".png"}
+                imageSrc={returnImageAccordingToCategory(category.url)}
                 categories={categories}
                 key={category.id}
                 category={category}
@@ -134,9 +199,10 @@ export default function ListCategories() {
     <div className="container h-100 d-flex flex-column">
       <div className="row justify-content-center">
         <div>
-          <h1>Liste des catégories</h1>
+          <h1 className="font-weight-bold">Choisir une catégorie</h1>
         </div>
       </div>
+      <hr className="solid" />
       {isRequesting ? <Spinner /> : renderFirstRow()}
       {isRequesting ? null : renderSecondRow()}
       {isRequesting ? null : renderThirdRow()}
--- apps/front-react/src/common/list-categories/category-item/CategoryItem.jsx
+++ apps/front-react/src/common/list-categories/category-item/CategoryItem.jsx
@@ -2,18 +2,46 @@ import React from "react";
 import "./CategoryItem.css";
 import { useNavigate } from "react-router-dom";
 import Button from "react-bootstrap/Button";
+import Image from "react-bootstrap/Image";
+
+// const path = "../../../assets/png/categories/ai.png";
+
+// import AIFFF from path;
 
 export default function CategoryItem(props) {
   let navigate = useNavigate();
+  const titleImage = "ai";
+  const path = `../../../assets/png/categories/${titleImage}.png`;
+  const path2 = `../../../assets/png/categories/${titleImage}.png`;
 
   return (
     <div className="col col-4 ">
-      <div className="d-flex rounded my-1 justify-content-between align-content-center align-self-center shadow py-3 px-3">
+      <div
+        className="d-flex rounded noselect my-1 justify-content-between align-content-center align-self-center category-container  py-3 px-3"
+        onClick={() =>
+          navigate("/explorer/" + props.category.url, {
+            state: {
+              category: props.category,
+              categories: props.categories,
+            },
+          })
+        }
+      >
         <div className="d-flex align-items-center">
-          <h3 className="my-0">{props.category.name}</h3>
+          <h3 className="my-0 small font-weight-bold text-uppercase">
+            {props.category.name}
+          </h3>
         </div>
         <div>
-          <Button
+          <div className="d-flex align-items-center" style={{ height: "80px" }}>
+            <Image
+              src={props.imageSrc}
+              // src={require(path2)}
+              // src={require("../../../assets/png/categories/ai.png")}
+              style={{ height: "80%", width: "auto" }}
+            />
+          </div>
+          {/* <Button
             onClick={() =>
               navigate("/explorer/" + props.category.url, {
                 state: {
@@ -24,7 +52,7 @@ export default function CategoryItem(props) {
             }
           >
             {props.category.id}
-          </Button>
+          </Button> */}
         </div>
       </div>
     </div>
