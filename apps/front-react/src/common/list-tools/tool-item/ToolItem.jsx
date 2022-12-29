a071a11ada62caa5719c430815f96a8d5ffb9f73
chupinal <alexischup@gmail.com>
2022-12-29 19:23:45 +0100

    Style for tool page

--- apps/front-react/src/common/list-tools/tool-item/ToolItem.jsx
+++ apps/front-react/src/common/list-tools/tool-item/ToolItem.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./ToolItem.css";
 import { useNavigate } from "react-router-dom";
-import Button from "react-bootstrap/Button";
 import Image from "react-bootstrap/Image";
 import "../../list-categories/ListCategories.css";
 
